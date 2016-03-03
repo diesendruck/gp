225abfbca799ec1e87c6cbf5f5c2dc2282360cfb
diesendruck <moglobal@gmail.com>
2016-03-03 17:57:41 -0600

    mcmc GP posterior

---
+++ Functions/p3.m
@@ -0,0 +1,7 @@
+function [x, y] = p3(x, y)
+% Shortcut to scatterplot, for when you have a matrix of x, and response y.
+
+scatter3(x(:,1), x(:,2), y);
+
+end
+
--- Functions/run_gp.m
+++ Functions/run_gp.m
@@ -1,61 +1,66 @@
-function [xt, Eft_map] = run_gp(x, y, ls_factor, color)
-% Run gpstuff.
+function [xt, Eft] = run_gp(x, y, ls_factor, mcmc_or_map)
+% Run gpstuff to produce GP posterior surface.
+% Inputs:
+%   x: n x d matrix of data values.
+%   y: n x 1 matrix of response values.
+%   ls_factor: Prior value for lengthscale hyperparameter.
+%   color: Colormap selection, e.g. "winter" or "summer".
 
-% GP REGRESSION ON TRAINING DATA ------------------------------------------
-% Goal: Make GP regression data, finish with vectors x, y.
-
-% Establish boundary of data.
-x_min = min(min(x)); x_max = max(max(x)); y_min = min(y); y_max = max(y);
+% STEP 0. Establish boundary of data, to make grid for surface.
+grid_granularity = 20;
+x_min = min(min(x)); x_max = max(max(x));  % Min/max across d dimensions.
 x_range = x_max - x_min;
+x_grid = x_min*1.1:x_range/grid_granularity:x_max*1.1;  % Create surface grid.
+[xt1,xt2] = meshgrid(x_grid,x_grid);
+xt=[xt1(:) xt2(:)];
 
-% Step 1. Train the GP.
+% STEP 1. Train the GP.
+noise_var_factor = 0.01;
 length_scale = [x_range*ls_factor, x_range*ls_factor];  % Scaled according to range.
-mag_sig2 = (x_range*0.01)^2;
+mag_sig2 = (x_range*noise_var_factor)^2;  % Scaled according to range.
 
 lik = lik_gaussian('sigma2', mag_sig2);
 gpcf = gpcf_sexp('lengthScale', length_scale, 'magnSigma2', mag_sig2);
 
-pn=prior_logunif();
+pn=prior_logunif();  % All parameters get uniform prior.
 lik = lik_gaussian(lik, 'sigma2_prior', pn);
 pl = prior_unif();
 pm = prior_sqrtunif();
 
-gpcf = gpcf_sexp(gpcf, 'lengthScale_prior', pl, 'magnSigma2_prior', pm);
-gp = gp_set('lik', lik, 'cf', gpcf);
+gpcf = gpcf_sexp(gpcf, 'lengthScale_prior', pl, 'magnSigma2_prior', pm);  % Assemble covariance function.
+gp = gp_set('lik', lik, 'cf', gpcf);  % Assemble gaussian process.
 
-    %Should work now that X is multivariate
-    %[K, C] = gp_trcov(gp, x);
-    %example_x = [-1 -1 ; 0 0 ; 1 1];
-    %[K, C] = gp_trcov(gp, example_x)
+% STEP 2. Optimize GP and get params, MAP version.
+if strcmp(mcmc_or_map, 'MAP')
+    opt = optimset('TolFun', 1e-3, 'TolX', 1e-3, 'Display','iter');
+    gp = gp_optim(gp, x, y, 'opt', opt);
+    [w,s] = gp_pak(gp);
+    disp(s), disp(exp(w))
 
-% Step 2. Optimize GP and get params, MAP version.
-opt=optimset('TolFun',1e-3,'TolX',1e-3,'Display','iter');
-gp=gp_optim(gp,x,y,'opt',opt);
-[w,s] = gp_pak(gp);
-disp(s), disp(exp(w))
-% Step 2. Optimize GP and get params, MCMC version.  % --NOT WORKING YET--.
-%[gp_rec,g,opt] = gp_mc(gp, x, y, 'nsamples', 220);
-%gp_rec = thin(gp_rec, 21, 2);
+    % STEP 3. Produce surface prediction.
+    [Eft_map, Varft_map] = gp_pred(gp, x, y, xt);
+    z = reshape(Eft_map, size(xt1));
+    Eft = Eft_map;
 
-% Step 3. Create surface grid.
-x_grid = x_min*1.1:x_range/20:x_max*1.1;
-[xt1,xt2]=meshgrid(x_grid,x_grid);  %Scaled according to range.
-xt=[xt1(:) xt2(:)];
+% STEP 2. Optimize GP and get params, MCMC version.
+elseif strcmp(mcmc_or_map, 'MCMC')
+    [gp_rec, g, opt] = gp_mc(gp, x, y, 'nsamples', 120);
+    gp_rec = thin(gp_rec, 21, 2);
+    
+    % STEP 3. Produce surface prediction.
+    % [Eft_s, Varft_s] = gpmc_preds(rfull, x, y, xt);  % TODO: What is rfull?
+    [Eft_mc, Varft_mc] = gp_pred(gp_rec, x, y, xt);
+    z = reshape(Eft_mc, size(xt1));
+    Eft = Eft_mc;
 
-% Step 4. Produce surface prediction, MAP version.
-[Eft_map, Varft_map] = gp_pred(gp, x, y, xt);
-z = reshape(Eft_map, size(xt1));
-% Step 4. Produce surface prediction, MCMC version.  % --NOT WORKING YET--.
-%[Eft_s, Varft_s] = gpmc_preds(rfull, x, y, xt);
-%[Eft_mc, Varft_mc] = gp_pred(gp_rec, x, y, xt);
-
-% Step 4. Plot the surface, with the training data.
-surf(xt1, xt2, z, 'FaceColor','interp', 'EdgeColor','flat', 'FaceLighting','gouraud');
-if color=='winter'
-    colormap winter;
 else
-    colormap summer;
+    error('Select either "MCMC" or "MAP" inference')
+
 end
+
+% STEP 4. Plot the surface, with the training data.
+surf(xt1, xt2, z, 'FaceColor','interp', 'EdgeColor', 'flat', ...
+    'FaceLighting','gouraud');
 axis tight; hold on;
 plot3(x(:,1), x(:,2), y, 'r.', 'MarkerSize', 40);
 

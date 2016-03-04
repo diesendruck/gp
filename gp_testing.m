5ca7a4c42e22f0090521e7c356d53e564a34a61e
diesendruck <moglobal@gmail.com>
2016-03-03 19:53:50 -0600

    trio graph

--- gp_testing.m
+++ gp_testing.m
@@ -6,19 +6,21 @@
 %   Available at: http://www.stat.columbia.edu/~bodhi/Bodhi/Publications.html
 
 % IMPORT GPSTUFF AND SET PATHS.
-cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/GPstuff-4.6/
-matlab_install
-addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/')
-addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Programs')
-addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Functions')
-cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/
+if 0
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/GPstuff-4.6/
+    matlab_install
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Programs')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Functions')
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/
+end
 
 % SET CONSTANTS.
 tol_thres = 0;
 eps1 = 10^-5;      % These 2 epsilons are used for convergence of the algo.
 eps2 = 10^-5;
 iter = 0;          % Counter for iterations
-n = 25;            % Sample size
+n = 10;            % Sample size
 d = 2;             % Dimension d
 sig = 10.0;         % Error variance
 ls_factor = 0.06;  % Lengthscale factor (proportion of x-range)
@@ -30,19 +32,19 @@ ls_factor = 0.06;  % Lengthscale factor (proportion of x-range)
 
 % RUN GP ON RAW DATA.
 figure;
-subplot(1, 2, 1)
-[xt_gp, y_gp] = run_gp(x_raw, y_raw, ls_factor, 'MCMC');  % Plots surface.
-
+subplot(1, 3, 1)
+[xt_gp, y_gp] = run_gp(x_raw, y_raw, ls_factor, 'MAP');
+subplot(1, 3, 2)
+[xt_gp, y_gp] = run_gp(x_raw, y_raw, ls_factor, 'MCMC');
 % GET CONVEX PROJECTION.
 n_gp = length(xt_gp);
-convex_y = project_to_convex(n_gp, d, xt_gp, y_gp, eps1, eps2, Max_Iter,...
-    rho);
+convex_y = project_to_convex(n_gp, d, xt_gp, y_gp, eps1, eps2, Max_Iter, rho);
 %y-convex_y
 
 % PLOT CONVEX OVER ORIGINAL GP.
 [xq, yq] = meshgrid(-10:.2:10);
 vq = griddata(xt_gp(:,1), xt_gp(:,2), convex_y, xq, yq);
-subplot(1,2,2)
+subplot(1, 3, 3)
 mesh(xq,yq,vq);
 hold on
 plot3(x_raw(:,1), x_raw(:,2), y_raw, 'b.', 'MarkerSize', 40);

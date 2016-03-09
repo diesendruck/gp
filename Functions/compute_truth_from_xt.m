6989502af864338944629a5f346f40f446d712dc
md36769 <momod@utexas.edu>
2016-03-08 21:15:43 -0600

    added compute_truth_from_xt

---
+++ Functions/compute_truth_from_xt.m
@@ -0,0 +1,21 @@
+function [ytruth_on_mcmcgrid] = compute_truth_from_xt(xt, shape)
+% Evaluate true convex function on xt grid (from run_gpmc).
+
+n = length(xt);
+ytruth_on_mcmcgrid = zeros(length(xt), 1);
+
+if strcmp(shape, 'trough')  % Convex wrt first dim of x. Other variable is random depth.
+    for ii = 1:n
+        ytruth_on_mcmcgrid(ii) = xt(ii, 1)^2;            
+    end
+    
+elseif strcmp(shape, 'paraboloid')
+    for jj = 1:n
+        ytruth_on_mcmcgrid(jj) = norm(xt(jj, :))^2;
+    end
+else
+    error('Shape should be either "trough" or "paraboloid".')
+end
+
+end
+

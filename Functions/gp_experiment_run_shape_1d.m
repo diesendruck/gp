a08b3dbd0d5ddaa4ff55ce08f1b10089d86f2d59
diesendruck <moglobal@gmail.com>
2016-03-17 15:20:48 -0500

    experimental setup for 1d

---
+++ Functions/gp_experiment_run_shape_1d.m
@@ -0,0 +1,98 @@
+function [xq, yq, zq_proj] = gp_experiment_run_shape(tol_thres, eps1, ...
+    eps2, iter, n, ls_factor, mesh_gran, num_posteriors, desired, d, ...
+    shape, fid)
+% Run gp experiment* for a particular shape.
+%
+% *One experiment draws many samples from the Gaussian Process posterior,
+% projects each one, then compares the posterior average to the projection
+% average.
+%
+% Args:
+%   tol_thres: ?
+%   eps1: These 2 epsilons are used for convergence of the convex 
+%     projection algorithm.
+%   eps2: Same as above.
+%   iter: Counter for iterations.
+%   n: Data sample size.
+%   ls_factor: Lengthscale factor (proportion of x-range).
+%   mesh_gran: Number of ticks on mesh for plotting.
+%   num_posteriors: Number of posterior samples to generate.
+%   desired: Number of posterior samples to use.
+%   d: Dimension of data points.
+%   shape: String, describing original convex function.
+%   fid: File ID, used to store mses for average mcmc and projection.
+%
+% Returns:
+%   xq: x-component of meshgrid for querying the surface.
+%   yq: y-component of meshgrid for querying the surface.
+%   zq_proj: Response value component of meshgrid for projection surface.
+
+%% SIMULATE RAW DATA (CONVEX + NOISE).
+[x_nsy, y_nsy, x1_l, x1_h, x2_l, x2_h, x1_range, x2_range] = make_noisy_convex(...
+    n, d, shape);
+
+%% GET SAMPLES FROM GP POSTERIOR MCMC, PROJECT EACH TO CONVEX, AND STORE.
+[xt1, xt2, xt, Eft_s, posterior_sample_count] = run_gpmc(x_nsy, y_nsy, ...
+    ls_factor, num_posteriors);
+n_gp = length(xt);
+
+n_entries = min(desired, posterior_sample_count); 
+mcmcs = zeros(n_gp, n_entries);
+projs = zeros(n_gp, n_entries);
+
+for index = 1:n_entries
+    disp(sprintf('(%s) Projecting convex version of sample %d.', shape, ...
+        index));
+    % Sample once from posterior, and store it as a column in mcmcs.
+    y_smp = Eft_s(:, randi(posterior_sample_count));
+    mcmcs(:, index) = y_smp;
+    % Get convex projection of sample, and store it as a column in projs.
+    y_smp_convex = project_to_convex(n_gp, d, xt, y_smp, eps1, eps2);
+    projs(:, index) = y_smp_convex;
+end
+
+%% COMPUTE AVERAGES OVER MCMC AND CONVEX PROJECTIONS, RESPECTIVELY.
+avg_mcmcs = mean(mcmcs, 2);  % Row means.
+avg_projs = mean(projs, 2);
+
+%% COMPUTE MSE BETWEEN TRUTH AND EACH AVERAGE.
+ytruth_on_mcmcgrid = compute_truth_from_xt(xt, shape);
+mcmc_mse = 1/n_gp * norm(avg_mcmcs - ytruth_on_mcmcgrid)^2;
+proj_mse = 1/n_gp * norm(avg_projs - ytruth_on_mcmcgrid)^2;
+relative_change = (proj_mse - mcmc_mse)/mcmc_mse;
+mses = [mcmc_mse proj_mse relative_change];
+
+%% PLOT TRUE CONVEX OVER ORIGINAL DATA.
+% Compute meshgrid given data. Use this in all graphs.
+[xq, yq] = meshgrid(x1_l:x1_range/mesh_gran:x1_h, x2_l:x2_range/mesh_gran:x2_h);
+zq_conv = griddata(xt(:, 1), xt(:, 2), ytruth_on_mcmcgrid, xq, yq);
+figure; subplot(1, 3, 1);
+mesh(xq, yq, zq_conv); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title('True Convex');
+
+%% PLOT AVG MCMC OVER ORIGINAL DATA.
+zq_mcmc = griddata(xt(:, 1), xt(:, 2), avg_mcmcs, xq, yq);
+subplot(1, 3, 2);
+mesh(xq, yq, zq_mcmc); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Avg MCMC (MSE = %d)', mcmc_mse));
+
+%% PLOT AVG PROJ OVER ORIGINAL DATA.
+zq_proj = griddata(xt(:, 1), xt(:, 2), avg_projs, xq, yq);
+subplot(1, 3, 3);
+mesh(xq, yq, zq_proj); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Avg PROJ (MSE = %d)', proj_mse));
+
+%% SAVE FILE DATA AND FIGURE.
+fprintf(fid, '%d,%d,%d\n', mses);
+csvwrite(sprintf('%s_x_nsy.csv', shape), x_nsy);
+csvwrite(sprintf('%s_y_nsy.csv', shape), y_nsy);
+csvwrite(sprintf('%s_xq.csv', shape), xq);
+csvwrite(sprintf('%s_yq.csv', shape), yq);
+csvwrite(sprintf('%s_zq_proj.csv', shape), zq_proj);
+savefig(sprintf('%s.fig', shape));
+
+end
+
---
+++ gp_experiment_1d.m
@@ -0,0 +1,111 @@
+% This version was partially authored by Maurice Diesendruck.
+
+% Adapted from Demo_example.m, a part of:
+% Mazumder, R., Choudhury, A., Iyengar, G. and Sen, B. (2015).
+%   A Computational Framework for Multivariate Convex Regression and its Variants.
+%   Available at: http://www.stat.columbia.edu/~bodhi/Bodhi/Publications.html
+
+tic
+
+%% IMPORT GPSTUFF AND SET PATHS.
+if 0  % Mac version.
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/GPstuff-4.6/
+    matlab_install
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/Programs/
+    run_mex_commands();
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Programs')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Functions')
+end
+
+if 1  % Linux version.
+    cd ~/Documents/gp/GPstuff-4.6/
+    matlab_install
+    cd ~/Documents/gp/Programs/
+    run_mex_commands();
+    cd ~/Documents/gp/
+    addpath('~/Documents/gp/')
+    addpath('~/Documents/gp/Programs')
+    addpath('~/Documents/gp/Functions')
+end
+
+%% SET CONSTANTS.
+tol_thres = 0;
+eps1 = 10^-5;          % These 2 epsilons are used for convergence of the
+eps2 = 10^-5;          %   convex projection algorithm.
+iter = 0;              % Counter for iterations.
+n = 20;                % Data sample size.
+d = 1;                  % Dimension of data points.
+ls_factor = 0.01;      % Lengthscale factor (proportion of x-range).
+mesh_gran = 30;       % Number of ticks on mesh for plotting.
+num_posteriors = 1020;  % Number of posterior samples to generate.
+desired = 10;           % Number of posterior samples to use.
+
+%% SETUP EMAIL PARAMS.
+myaddress = 'eltegog@gmail.com';
+mypassword = 'T0g.eltegog';
+setpref('Internet','E_mail',myaddress);
+setpref('Internet','SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username',myaddress);
+setpref('Internet','SMTP_Password',mypassword);
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', ...
+    'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+%% SAVE MSE RESULTS TO FILE.
+fid = fopen('Results_1d/mses.csv', 'wt');
+fprintf(fid, 'avg_mcmc_mse,avg_proj_mse,relative_change\n');
+
+%% CONDUCT EXPERIMENT ON EACH SHAPE.
+% List of shapes to run.
+shapes = {'parabola'};
+
+% Run experiment for each shape.
+for shape = shapes
+    gp_experiment_run_shape(tol_thres, eps1, eps2, iter, n, ls_factor, ...
+        mesh_gran, num_posteriors, desired, d, shape{1}, fid);
+    sendmail('momod@utexas.edu', 'UPDATES', sprintf('Finished %s', shape{1}));
+end
+
+fclose(fid);
+
+
+sendmail('momod@utexas.edu', 'RESULTS', '', ...
+    {'/home/momod/Documents/gp/mses.csv', ...
+    '/home/momod/Documents/gp/trough.fig', ...
+    '/home/momod/Documents/gp/trough_xq.csv', ...
+    '/home/momod/Documents/gp/trough_yq.csv', ...
+    '/home/momod/Documents/gp/trough_zq_proj.csv', ...  
+    '/home/momod/Documents/gp/paraboloid.fig', ...
+    '/home/momod/Documents/gp/paraboloid_xq.csv', ...
+    '/home/momod/Documents/gp/paraboloid_yq.csv', ...
+    '/home/momod/Documents/gp/paraboloid_zq_proj.csv', ...    
+    '/home/momod/Documents/gp/hand.fig', ...
+    '/home/momod/Documents/gp/hand_xq.csv', ...
+    '/home/momod/Documents/gp/hand_yq.csv', ...
+    '/home/momod/Documents/gp/hand_zq_proj.csv', ...
+    '/home/momod/Documents/gp/parabolic_cylinder.fig', ...
+    '/home/momod/Documents/gp/parabolic_cylinder_xq.csv', ...
+    '/home/momod/Documents/gp/parabolic_cylinder_yq.csv', ...
+    '/home/momod/Documents/gp/parabolic_cylinder_zq_proj.csv', ...
+    '/home/momod/Documents/gp/wolverine.fig', ...
+    '/home/momod/Documents/gp/wolverine_xq.csv', ...
+    '/home/momod/Documents/gp/wolverine_yq.csv', ...
+    '/home/momod/Documents/gp/wolverine_zq_proj.csv', ...
+    '/home/momod/Documents/gp/exponential.fig', ...
+    '/home/momod/Documents/gp/exponential_xq.csv', ...
+    '/home/momod/Documents/gp/exponential_yq.csv', ...
+    '/home/momod/Documents/gp/exponential_zq_proj.csv', ...
+    '/home/momod/Documents/gp/chair.fig', ...
+    '/home/momod/Documents/gp/chair_xq.csv', ...
+    '/home/momod/Documents/gp/chair_yq.csv', ...
+    '/home/momod/Documents/gp/chair_zq_proj.csv'});
+    
+toc
+
+
+
+    

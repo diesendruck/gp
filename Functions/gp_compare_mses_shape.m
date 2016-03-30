6bd988c40c53cd4e79ba2028ce20b4d9d1fb8ea8
md36769 <momod@utexas.edu>
2016-03-30 17:54:18 -0500

    changed scripts to compare methods on same dataset

---
+++ Functions/gp_compare_mses_shape.m
@@ -0,0 +1,133 @@
+function [mse_gp, mse_gp_proj, mse_kern, mse_kern_proj] = ...
+    gp_compare_mses_shape(tol_thres, eps1, eps2, iter, n, ls_factor, ...
+    mesh_gran, num_posteriors, desired, d, shape, fid)
+% Run gp experiment* for a particular shape.
+%
+% *One experiment draws many samples from the Gaussian Process posterior,
+% projects each one, then compares the posterior average to the projection
+% average.
+%
+% Args:
+%   tol_thres: ?
+%   eps1: These 2 epsilons are used for convergence of the convex 
+%       projection algorithm.
+%   eps2: Same as above.
+%   iter: Counter for iterations.
+%   n: Data sample size.
+%   ls_factor: Lengthscale factor (proportion of x-range).
+%   mesh_gran: Number of ticks on mesh for plotting.
+%   num_posteriors: Number of posterior samples to generate.
+%   desired: Number of posterior samples to use.
+%   d: Dimension of data points.
+%   shape: String, describing original convex function that produced data.
+%   fid: File ID, used to store mses for average mcmc and projection.
+%
+% Returns:
+%   mse_gp: MSE of average of desired number of GP posteriors.
+%   mse_gp_proj: MSE of average of projections of desired number of GP posteriors.
+%   mse_kern: MSE of kernel regression over raw data.
+%   mse_kern_proj: MSE of projection of kernel regression.
+
+
+%% SIMULATE RAW DATA (CONVEX + NOISE).
+[x_nsy, y_nsy] = make_noisy_convex(n, d, shape);
+
+% Get associated data about noisy data set.
+[~, ~, ~, ~, ~, ~, xt1, xt2, xt] = compute_mesh_info(x_nsy);
+
+% Plot true convex over original data.
+ytruth_on_grid = compute_truth_from_xt(xt, shape);
+yq_conv = griddata(xt(:, 1), xt(:, 2), ytruth_on_grid, xt1, xt2);
+figure; subplot(2, 3, 1);
+mesh(xt1, xt2, yq_conv); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title('True Convex');
+
+
+%% COMPUTE KERNEL REGRESSION, ITS PROJECTION, AND RESPECTIVE MSES.
+% Select optimal bandwidth, and do kernel regression.
+h0 = rand(size(x_nsy', 1), 1)*10;
+fprintf('(%s) Optimizing for kernel regression bandwidth.', shape);
+h = Opt_Hyp_Gauss_Ker_Reg(h0, x_nsy', y_nsy');
+y_kern = zeros(size(xt1));
+for ii = 1:size(xt1, 1)
+    for jj = 1:size(xt1, 2)
+        xk = [xt1(ii, jj); xt2(ii, jj)];
+        y_kern(ii, jj) = gaussian_kern_reg(xk, x_nsy', y_nsy', h);
+    end
+end
+
+% Get convex projection of kernel regression.
+fprintf('(%s) Projecting kernel regression surface to convex.', shape);
+y_kern_proj = project_to_convex(length(xt), d, xt, y_kern(:), eps1, eps2);
+
+% Compute mses and relative change
+mse_kern = 1/length(xt) * norm(y_kern(:) - ytruth_on_grid)^2;
+mse_kern_proj = 1/length(xt) * norm(y_kern_proj - ytruth_on_grid)^2;
+
+% Plot kernel regression and convex projection over original data.    
+subplot(2, 3, 2);
+mesh(xt1, xt2, y_kern); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Kernel Regression (MSE = %d)', mse_kern));
+
+subplot(2, 3, 3);
+yq_proj = griddata(xt(:, 1), xt(:, 2), y_kern_proj, xt1, xt2);
+mesh(xt1, xt2, yq_proj); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Projection (MSE = %d)', mse_kern_proj));
+
+
+%% COMPUTE AVERAGE FROM SAMPLES OF GP POSTERIOR MCMC, AVERAGE OF 
+%% PROJECTIONS OF EACH, AND MSES OF RESPECTIVE AVERAGES.
+% Get samples from GP posterior MCMC, project each to convex, and store.
+[xt1, xt2, xt, Eft_s, posterior_sample_count] = run_gpmc(x_nsy, ...
+    y_nsy, ls_factor, num_posteriors);
+n_gp = length(xt);
+
+n_entries = min(desired, posterior_sample_count); 
+mcmcs = zeros(n_gp, n_entries);
+projs = zeros(n_gp, n_entries);
+
+for index = 1:n_entries
+    fprintf('(%s) Projecting mcmc surface to convex, sample %d.\n', ...
+        shape, index);
+    % Sample once from posterior, and store it as a column in mcmcs.
+    y_smp = Eft_s(:, randi(posterior_sample_count));
+    mcmcs(:, index) = y_smp;
+    % Get convex projection of sample, and store it as a column in projs.
+    y_smp_convex = project_to_convex(n_gp, d, xt, y_smp, eps1, eps2);
+    projs(:, index) = y_smp_convex;
+end
+
+% Compute averages over mcmc and convex projections, respectively.
+avg_mcmcs = mean(mcmcs, 2);  % Row means.
+avg_projs = mean(projs, 2);
+
+% Compute MSE between truth and each average.
+mse_gp = 1/n_gp * norm(avg_mcmcs - ytruth_on_grid)^2;
+mse_gp_proj = 1/n_gp * norm(avg_projs - ytruth_on_grid)^2;
+
+% Plot avg MCMC over original data.
+yq_mcmc = griddata(xt(:, 1), xt(:, 2), avg_mcmcs, xt1, xt2);
+subplot(2, 3, 5);
+mesh(xt1, xt2, yq_mcmc); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Avg MCMC (MSE = %d)', mse_gp));
+
+% Plot avg proj over original data..
+yq_proj = griddata(xt(:, 1), xt(:, 2), avg_projs, xt1, xt2);
+subplot(2, 3, 6);
+mesh(xt1, xt2, yq_proj); hold on;
+plot3(x_nsy(:, 1), x_nsy(:, 2), y_nsy, 'r.', 'MarkerSize', 30);
+title(sprintf('Avg PROJ (MSE = %d)', mse_gp_proj));
+
+
+%% SAVE FILE DATA AND FIGURE.
+fprintf(fid, '%s,%s,%s,%s,%s\n', shape, num2str(mse_gp, '%0.7f'), ...
+                                 num2str(mse_gp_proj, '%0.7f'), ...
+                                 num2str(mse_kern, '%0.7f'), ...
+                                 num2str(mse_kern_proj, '%0.7f')); 
+
+end
+
---
+++ gp_compare_mses.m
@@ -0,0 +1,115 @@
+% This version was partially authored by Maurice Diesendruck.
+
+% Adapted from Demo_example.m, a part of:
+% Mazumder, R., Choudhury, A., Iyengar, G. and Sen, B. (2015).
+%   A Computational Framework for Multivariate Convex Regression and its Variants.
+%   Available at: http://www.stat.columbia.edu/~bodhi/Bodhi/Publications.html
+
+start_time = tic
+
+%% IMPORT GPSTUFF AND SET PATHS.
+if 0  % Mac version.
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/GPstuff-4.6/
+    matlab_install
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/Programs/
+    run_mex_commands();
+    cd ~/Google' Drive'/0-LIZHEN' RESEARCH'/gp/
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Programs')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Functions')
+    addpath('~/Google Drive/0-LIZHEN RESEARCH/gp/Smoothing')
+end
+
+if 1  % Linux version.
+    cd ~/Documents/gp/GPstuff-4.6/
+    matlab_install
+    cd ~/Documents/gp/Programs/
+    run_mex_commands();
+    cd ~/Documents/gp/
+    addpath('~/Documents/gp/')
+    addpath('~/Documents/gp/Programs')
+    addpath('~/Documents/gp/Functions')
+    addpath('~/Documents/gp/Smoothing')
+end
+
+%% SET CONSTANTS.
+format long;
+tol_thres = 0;
+eps1 = 10^-5;          % These 2 epsilons are used for convergence of the
+eps2 = 10^-5;          %   convex projection algorithm.
+iter = 0;              % Counter for iterations.
+n = 40;                % Data sample size.
+d = 2;                  % Dimension of data points.
+ls_factor = 0.01;      % Lengthscale factor (proportion of x-range).
+mesh_gran = 20;        % Number of ticks on mesh for plotting.
+num_posteriors = 120; % Number of posterior mcmc samples to generate.
+desired = 2;         % Number of posterior mcmc samples to use.
+num_global_iters = 2; % Number of MSEs to produce per shape.
+
+%% SETUP EMAIL PARAMS.
+myaddress = 'eltegog@gmail.com';
+myp = 'T0g.eltegog';
+setpref('Internet','E_mail',myaddress);
+setpref('Internet','SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username',myaddress);
+setpref('Internet','SMTP_Password',myp);
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', ...
+    'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+%% SAVE MSE RESULTS TO FILE.
+fid = fopen('Results_2d/mses.csv', 'wt');
+% Set up for one global run.
+% fprintf(fid, 'avg_mcmc_mse,avg_proj_mse,relative_change\n');
+% Set up for multiple global runs on three shapes.
+fprintf(fid, 'data_shape,gp,gp_proj,kern,kern_proj\n');
+
+
+%% CONDUCT EXPERIMENT ON EACH SHAPE.
+% List of shapes to run.
+% shapes = {'trough', 'paraboloid', 'hand', 'parabolic_cylinder', ...
+%           'wolverine', 'exponential', 'chair'};
+% Try with only "flatter" surfaces.
+shapes = {'hand', 'parabolic_cylinder', 'wolverine'};
+
+% Run experiment for each shape.
+for ii = 1:num_global_iters
+    for shape = shapes
+        shape_start_time = tic;
+        
+        [gp, gp_proj, kern, kern_proj] = gp_compare_mses_shape(tol_thres, ...
+            eps1, eps2, iter, n, ls_factor, mesh_gran, num_posteriors, ...
+            desired, d, shape{1}, fid);
+        
+        shape_time_elapsed = toc(shape_start_time);
+        total_time_elapsed = toc(start_time);
+        
+        sendmail('momod@utexas.edu', 'UPDATE: MSE Comparisons', ...
+            sprintf(strcat('Global iter: \t%s\n', ...
+                           'Data shape: \t%s\n', ...
+                           'gp: \t%s\n', ...
+                           'gp_proj: \t%s\n', ... 
+                           'kern: \t%s\n', ...
+                           'kern_proj: \t%s\n', ...
+                           'shape_time: \t%s\n', ...
+                           'total_time_elapsed: \t%s\n'), ...
+                num2str(ii), shape{1}, num2str(gp, '%0.7f'), ...
+                num2str(gp_proj, '%0.2f'), num2str(kern, '%0.2f'), ...
+                num2str(kern_proj, '%0.2f'), ...
+                num2str(shape_time_elapsed, '%0.2f'), ...
+                num2str(total_time_elapsed, '%0.2f')));
+    end
+end
+
+fclose(fid);
+   
+sendmail('momod@utexas.edu', 'RESULTS: MSE Comparisons', '', ...
+    {'/home/momod/Documents/gp/Results_2d/mses.csv'});
+
+end_time = toc(start_time)
+
+
+
+    
